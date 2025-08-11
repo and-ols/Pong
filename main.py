f4a9133481896af0b085f886eff28e9c506dc7bf
Andrew Olson <ols21007@byui.edu>
2025-08-11 12:03:09 -0600

    Set up screen

---
+++ main.py
@@ -0,0 +1,11 @@
+from turtle import Screen
+
+# Screen setup
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+screen.tracer(0)
+
+
+screen.exitonclick()
